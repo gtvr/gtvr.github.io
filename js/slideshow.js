1e272ca6adad9e58e8e5463d854f568d4524863a
M. A. Rizvi <mrizvi1996@gmail.com>
2016-04-25 19:25:43 -0400

    added slideshow

---
+++ js/slideshow.js
@@ -0,0 +1,33 @@
+var slideIndex = 1;
+var selected = false;
+carousel();
+//showDivs(slideIndex);
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    if (!selected) {setTimeout(carousel, 3000);} // Change image every 2 seconds
+}
+
+function plusDivs(n) {
+    selected = true;
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
