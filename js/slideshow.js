7b8f55a140cc6f57fa236182eba7bd2103ce2c39
M. A. Rizvi <mrizvi1996@gmail.com>
2016-04-25 19:29:39 -0400

    fix2

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,19 +1,6 @@
-var slideIndex = 1;
+var slideIndex = 0;
 var selected = false;
-carousel();
-//showDivs(slideIndex);
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    if (!selected) {setTimeout(carousel, 3000);} // Change image every 2 seconds
-}
+showDivs(slideIndex);
 
 function plusDivs(n) {
     selected = true;
