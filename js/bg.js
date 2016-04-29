9dd6129e9df00fe0e3b84185128171fc8789bb0d
EntitySK <m.rizvi@arsmachina.net>
2016-04-29 16:48:18 -0400

    tweak2

--- js/bg.js
+++ js/bg.js
@@ -1,23 +1,33 @@
-var radius = 100, maxSize = 20, minSize = 1;
+var radius = 300, maxSize = 20, minSize = 1, slope = 0.35, rate = 10;
+
+var scan = 0, maxScan = 0;
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    fill(230, 225, 255);
+    fill(240, 235, 255);
     noStroke();
 }
 
+var scan;
+
 function draw() {
+    maxScan = width + width * (1 - slope) + radius;
+    
     clear();
     
-    for(var i = 0; i <= width; i += maxSize) {
+    for(var i = 0; i <= maxScan; i += maxSize) {
 	for(var j = 0; j <= height; j += maxSize) {
-	    var d = (radius - dist(mouseX, mouseY, i, j)) / radius;
+	    var d = (radius - dist(scan, 0, i, 0)) / radius;
 	    if (d >= 0) {
 		var size = ((maxSize - minSize) * d) + minSize;
-		rect(i, j, size, size, 0, 0, maxSize, 0);
+		rect(i - slope * j, j, size, size, 0, 0, maxSize, 0);
 	    }
 	}
     }
+    
+    scan += rate;
+    if (scan > maxScan)
+	scan = 0;
 }
 
 function windowResized() {
