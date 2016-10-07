080223ffd1123960c2da182aaf776f72bbba05c0
M. A. Rizvi <mrizvi1996@gmail.com>
2016-10-07 15:06:48 -0400

    splash code

---
+++ js/splash.js
@@ -0,0 +1,20 @@
+var splashes = new Array(
+    "Headsets & Dragons",
+    "Reality++",
+    "OMG",
+    "Why didn't we have one before!",
+    "Memes & Dreams",
+    "IPv7",
+    "The Metaverse",
+    "We're not VGDev",
+    "VR & VR Accessories",
+    "此の街",
+    "High Tension!",
+    "Who Needs Monitors‽",
+    "All of this is Real",
+    "The net is vast and infinite",
+    "Infinite Diversity in Infinite Combinations",
+    "2 Guys and a Headset",
+);
+
+document.getElementById('splash').innerHTML = splashes[Math.floor(Math.random() * splashes.length)];;
